c5a1332c580b4bc8a9ade0befd210373182dffe1
RyanMcGlinnGDD <ryanmcglinngdd@gmail.com>
2019-02-27 13:27:14 -0500

    Recomitted modern version of portfolio
    Complete overhaul applied to portfolio repository.

---
+++ js/main.js
@@ -0,0 +1,225 @@
+
+
+this.data = [
+  {
+    "title": "I-CORPS",
+    "subtitle": "JavaScript • HTML5 Canvas",
+    "showcaseImage": "./images/showcase/icorps.png",
+    "showcaseDescription": "Narrative-driven adventure game framework",
+    "lightboxDescription": "Research Project • Solo Development<br><br>I-CORPS is a narrative-driven adventure game framework designed to be the foundation for games where players assume the role of a detective and investigate a mystery via a dynamic progression system.<br><br>How it works:<br>The I-CORPS dynamic progression system is based on the concept of conspiracy boards in detective shows. Drawing connections between pieces of evidence can lead to new dialogues, crime scenes, and evidence. Fulfilling specific conditions will lead to one of potentially several end points.<br><br>Use cases:<br>I-CORPS was developed to explore open-ended narrative design, with the ability  to create branching narrative with variable outcomes.<br><br>The framework was developed for web deployment and relies on minimal dependencies.",
+    "lightboxThumb1": "./images/icorps/01.jpg",
+    "lightboxThumb2": "./images/icorps/02.jpg",
+    "lightboxThumb3": "./images/icorps/03.jpg",
+    "lightboxThumb4": "./images/icorps/04.jpg",
+    "lightboxImage1": "./images/icorps/01.jpg",
+    "lightboxImage2": "./images/icorps/02.jpg",
+    "lightboxImage3": "./images/icorps/03.jpg",
+    "lightboxImage4": "./images/icorps/04.jpg",
+    "lightboxLinks": [
+      {
+        "title": "Demo Part 1",
+        "icon": "./images/icon/logoDemo.png",
+        "link": "https://ryanmcglinngdd.github.io/I-CORPS-Demo-Part-1/"
+      },
+      {
+        "title": "Demo Part 2",
+        "icon": "./images/icon/logoDemo.png",
+        "link": "https://ryanmcglinngdd.github.io/I-CORPS-Demo-Part-2/"
+      }
+    ]
+  },
+  {
+    "title": "I.P.A.R.",
+    "subtitle": "C# • WPF",
+    "showcaseImage": "./images/showcase/IPAR.png",
+    "showcaseDescription": "Module-based training application",
+    "lightboxDescription": "Professional Project • Team Development<br><br>I.P.A.R. is a narrative-based detective game initially designed to introduce entry-level digital forensics students to real-world digital forensics concepts through modular \"cases\".<br><br>How it works:<br>A linear progression of nodes walk users through procedures that simulate field experience. Guided by step-by-step instructions presented in the lens of case narrative, files provided with each case are processed to progress. Players can be prompted to periodically confirm their progress via multiple choice or text entry, with reports compiled at the conclusion for instructor review.<br><br>Use cases:<br>As development proceeded, it became apparent that the I.P.A.R. project framework had potential beyond just the field of digital forensics. The means by which I.P.A.R. guides users can be used to tutorialize any topic at the entry-level. A presentation on this topic was presented at the NSF annual conference in 2016.<br><br>Since the target user demographic of I.P.A.R. is instructors and students that may not have coding experience, an early development target was to make an accompanying set of easy-to-use creation tools. The resulting tools enabled all users to build fully featured content modules using an intuitive interface with no scripting or code needed.<br><br>I.P.A.R. was deployed at select entry-level digital forensics courses at RIT and a network of community colleges in New York State.",
+    "lightboxThumb1": "./images/ipar/01.jpg",
+    "lightboxThumb2": "./images/ipar/02.jpg",
+    "lightboxThumb3": "./images/ipar/03.jpg",
+    "lightboxThumb4": "./images/ipar/04.jpg",
+    "lightboxImage1": "./images/ipar/01.jpg",
+    "lightboxImage2": "./images/ipar/02.jpg",
+    "lightboxImage3": "./images/ipar/03.jpg",
+    "lightboxImage4": "./images/ipar/04.jpg",
+    "lightboxLinks": [
+      {
+        "title": "Website",
+        "icon": "./images/icon/logoResume.png",
+        "link": "https://forensic-games.csec.rit.edu/"
+      },
+      {
+        "title": "Video",
+        "icon": "./images/icon/logoYoutube.png",
+        "link": "https://www.youtube.com/watch?v=Oq8GtXyUz1A"
+      }
+    ]
+  },
+  {
+    "title": "X-Translate",
+    "subtitle": "JavaScript",
+    "showcaseImage": "./images/showcase/x-translate.png",
+    "showcaseDescription": "Automated game localization tool",
+    "lightboxDescription": "Independent Project • Team Development<br>(this project is currently in active development)<br><br>Pronounced \"Cross Translate\", this tool is developed to assist human language translators tasked with translating large volumes of consistently worded game information.<br><br>How it works:<br>X-Translate operates by parsing and organizing a structured database of game text. Formatting rules and key terminologies established by the human translator define the logic by which the text is divided and categorized, and recurring patterns are identified. <br><br>After all data of the original language is processed, a bank of simplified phrases including original contexts is presented to the human translator for manual translation. After being translated, phrases will then be recompiled into the equivalent of the orginal in the new language.<br><br>Use cases:<br>X-Translate is well suited to handle translation projects where wordings are written consistently to describe tasks and effects (a specific example being trading card game effect text). The tool is not designed to process any specific language and can theoretically operate using any source language and output in any language within the capabilities of the human translator.",
+    "lightboxThumb1": "./images/x-translate/01.jpg",
+    "lightboxThumb2": "./images/x-translate/02.jpg",
+    "lightboxThumb3": "./images/x-translate/03.jpg",
+    "lightboxThumb4": "./images/x-translate/04.jpg",
+    "lightboxImage1": "./images/x-translate/01.jpg",
+    "lightboxImage2": "./images/x-translate/02.jpg",
+    "lightboxImage3": "./images/x-translate/03.jpg",
+    "lightboxImage4": "./images/x-translate/04.jpg",
+    "lightboxLinks": [
+    ]
+  },
+  {
+    "title": "Hackbox Party Pack",
+    "subtitle": "JavaScript • Electron",
+    "showcaseImage": "./images/showcase/hackbox.png",
+    "showcaseDescription": "Reverse-engineered content toolkit",
+    "lightboxDescription": "Independent Project • Solo Development<br><br>Hackbox is designed to convert spreadsheets containing user generated data into structured content modules readable by the otherwise unmodified Jackbox Party Pack™ game for PC.<br><br>How it works:<br>The organizational structure of the existing content modules the game shipped with was carefully studied with the objective of mimicking style and formatting. After much trial and error, it was possible to bundle new data such that the game could process it normally.<br><br>Hackbox parses a specifically formatted spreadsheet provided by the user and exports a nested folder structure that can be copied to the game's content directory for use.",
+    "lightboxThumb1": "./images/hackbox/01.jpg",
+    "lightboxThumb2": "./images/hackbox/02.jpg",
+    "lightboxThumb3": "./images/hackbox/03.jpg",
+    "lightboxThumb4": "./images/hackbox/04.jpg",
+    "lightboxImage1": "./images/hackbox/01.jpg",
+    "lightboxImage2": "./images/hackbox/02.jpg",
+    "lightboxImage3": "./images/hackbox/03.jpg",
+    "lightboxImage4": "./images/hackbox/04.jpg",
+    "lightboxLinks": [
+    ]
+  }
+];
+
+const init = () => {
+  //compound new html5 elements
+  let dynamicCompound = "";
+  
+  //parse json that contains all of the showcase's information
+  for(let i = 0; i < data.length; i++){
+    dynamicCompound += `
+      <div class="showcase" data-index="${i}">
+        <img src="${data[i].showcaseImage}">
+        <div>
+          <p class="showcaseTitle">${data[i].title}</p>
+          <p class="showcaseSubtitle">${data[i].subtitle}</p>
+          <p class="showcaseInfo">${data[i].showcaseDescription}</p>
+        </div>
+      </div>`;
+  }
+  //apply compounded elements
+  document.getElementById("showcaseZone").innerHTML = dynamicCompound;
+  
+  // set Project Showcase text
+  document.getElementById("projectShowcaseText").innerHTML = "Project Showcase";
+  
+  //parse the showcase elements and assign click events that populate lightbox
+  const showcaseElements = document.getElementsByClassName("showcase");
+  for(let i = 0; i < showcaseElements.length; i++){
+    showcaseElements[i].addEventListener("click", function(){
+      document.getElementById("photo").setAttribute("data-index", this.dataset.index);
+    });
+    showcaseElements[i].addEventListener("click", showcaseClick.bind(this));
+  }
+  
+  //set lightbox blinder onclick to make it non visible
+  document.getElementById("lightboxBlinder").addEventListener("click", function(){
+    document.getElementById("lightboxLayer").className = "hiddenLayer";
+  });
+  document.getElementById("lightboxXButton").addEventListener("click", function(){
+    document.getElementById("lightboxLayer").className = "hiddenLayer";
+  });
+  document.getElementById("lightboxHorizontalBounds").addEventListener("click", function(){
+    document.getElementById("lightboxLayer").className = "hiddenLayer";
+  });
+  //prevent child from receiving parent click events
+  document.getElementById("lightboxContent").addEventListener("click", function(event){
+    event.stopPropagation();
+  });
+  
+  
+  //set lightbox image thumbnails to properly change lightboximage
+  document.getElementById("lightboxThumb1").addEventListener("click", function(){
+    thumbClick(0,1);
+  }.bind(this));
+  document.getElementById("lightboxThumb2").addEventListener("click", function(){
+    thumbClick(0,2);
+  }.bind(this));
+  document.getElementById("lightboxThumb3").addEventListener("click", function(){
+    thumbClick(0,3);
+  }.bind(this));
+  document.getElementById("lightboxThumb4").addEventListener("click", function(){
+    thumbClick(0,4);
+  }.bind(this));
+}
+
+//sets preview image to corresponding image based on thumbnail click
+const thumbClick = (dataIndex, thumbIndex) => {
+  const targetIndex = document.getElementById("photo").dataset.index;
+  
+  // set all thumb class to unselected
+  document.getElementById("lightboxThumb1").className = "unselectedThumbnail";
+  document.getElementById("lightboxThumb2").className = "unselectedThumbnail";
+  document.getElementById("lightboxThumb3").className = "unselectedThumbnail";
+  document.getElementById("lightboxThumb4").className = "unselectedThumbnail";
+  // set the clicked thumb class to selected
+  if(thumbIndex === 1){ 
+    document.getElementById("lightboxThumb1").className = "selectedThumbnail";
+    document.getElementById("lightboxImage").src = this.data[targetIndex].lightboxImage1;
+    document.getElementById("lightboxImageLink").href = this.data[targetIndex].lightboxImage1;
+  } else if(thumbIndex === 2){ 
+    document.getElementById("lightboxThumb2").className = "selectedThumbnail"; 
+    document.getElementById("lightboxImage").src = this.data[targetIndex].lightboxImage2;
+    document.getElementById("lightboxImageLink").href = this.data[targetIndex].lightboxImage2;
+  } else if(thumbIndex === 3){ 
+    document.getElementById("lightboxThumb3").className = "selectedThumbnail"; 
+    document.getElementById("lightboxImage").src = this.data[targetIndex].lightboxImage3;
+    document.getElementById("lightboxImageLink").href = this.data[targetIndex].lightboxImage3;
+  } else if(thumbIndex === 4){ 
+    document.getElementById("lightboxThumb4").className = "selectedThumbnail"; 
+    document.getElementById("lightboxImage").src = this.data[targetIndex].lightboxImage4;
+    document.getElementById("lightboxImageLink").href = this.data[targetIndex].lightboxImage4;
+  }
+}
+
+const showcaseClick = () => {
+  //store index to use as reference
+  const targetIndex = document.getElementById("photo").dataset.index;
+  //set lightbox variables
+  document.getElementById("lightboxTitle").innerHTML = this.data[targetIndex].title;
+  document.getElementById("lightboxSubtitle").innerHTML = this.data[targetIndex].subtitle;
+  document.getElementById("lightboxDescription").innerHTML = this.data[targetIndex].lightboxDescription;
+
+  // set src
+  document.getElementById("lightboxThumb1").src = this.data[targetIndex].lightboxThumb1;  
+  document.getElementById("lightboxThumb2").src = this.data[targetIndex].lightboxThumb2;
+  document.getElementById("lightboxThumb3").src = this.data[targetIndex].lightboxThumb3;
+  document.getElementById("lightboxThumb4").src = this.data[targetIndex].lightboxThumb4;
+  document.getElementById("lightboxImage").src  = this.data[targetIndex].lightboxImage1;
+  document.getElementById("lightboxImageLink").href = this.data[targetIndex].lightboxImage1;
+  
+  // set selection status
+  document.getElementById("lightboxThumb1").className = "selectedThumbnail";
+  document.getElementById("lightboxThumb2").className = "unselectedThumbnail";
+  document.getElementById("lightboxThumb3").className = "unselectedThumbnail";
+  document.getElementById("lightboxThumb4").className = "unselectedThumbnail";
+  
+  //parse the list of links and populate the list of links
+  const lightboxLinks = this.data[targetIndex].lightboxLinks;
+  let dynamicCompound = "";
+  for(let i = 0; i < lightboxLinks.length; i++){
+    dynamicCompound += `
+      <a href="${lightboxLinks[i].link}" target="_blank">
+        <div class="lightboxButton">
+          <img src="${lightboxLinks[i].icon}">
+          <p>${lightboxLinks[i].title}</p>
+        </div>
+      </a>`;
+  }
+  document.getElementById("lightboxButtonZone").innerHTML = dynamicCompound;
+  
+  //make lightbox visible
+  document.getElementById("lightboxLayer").className = "";
+}
+
+window.onload = init;
